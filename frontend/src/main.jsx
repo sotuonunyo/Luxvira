6224d419323eabfae7938de9555f7a26865a0394
sotuonunyo <s.otuonunyo@gmail.com>
2026-03-26 15:49:30 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Simple test page to confirm build works
-function App() {
-  return (
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>🕯️ Luxvira Scents</h1>
-      <p>Website is loading successfully! 🎉</p>
-      <p><i>Next: We'll add your product pages step by step.</i></p>
-    </div>
-  )
-}
-
-// Render the app to the <div id="root"> in index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
